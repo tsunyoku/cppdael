e646eb48fa689d2f69b1525bee4f696163ae672f
tsunyoku <tsunyoku>
2021-11-28 00:33:17 +0000

    better testing suite

--- bindings.cpp
+++ bindings.cpp
@@ -2,38 +2,9 @@
 #include <iostream>
 #include "rijndael.h"
 
-std::string remove_chars(const std::string str, char* charsToRemove)
-{
-    char c[str.length()+1];
-    const char *p = str.c_str();
-    unsigned int z=0, size = str.length();
-    unsigned int x;
-    bool rem=false;
-
-    for(x=0; x<size; x++)
-    {
-        rem = false;
-        for (unsigned int i = 0; charsToRemove[i] != 0; i++)
-        {
-            if (charsToRemove[i] == p[x])
-            {
-                rem = true;
-                break;
-            }
-        }
-        if (rem == false) c[z++] = p[x];
-    }
-
-    c[z] = '\0';
-    return std::string(c);
-}
-
 std::string decrypt(std::string buffer, std::string key, std::string iv)
 {
-    const std::string _result = decrypt_string(buffer, key, iv); // this probably doesn't need to be standalone
-    std::string result = remove_chars(_result, (char*)"\x14");
-
-    return result;
+    return decrypt_string(buffer, key, iv); // this probably doesn't need to be standalone
 }
 
 PYBIND11_MODULE(cppdael, handle) {
--- tests/example.py
+++ tests/example.py
@@ -4,6 +4,8 @@ from py3rijndael import RijndaelCbc, ZeroPadding
 import cppdael
 import timeit
 
+RUN_COUNT = 1000 # how many times it should run the func to get an accurate average
+
 key = "osu!-scoreburgr---------20211103"
 iv = b64decode("8cpBI5PTxOlfDqXN9dE2OBothneHAmQZlN99K6zEkxI=")
 cipher = b64decode("2rQUjbqnBfb5CdnfcdACG+IjeYlTkE9cHxD3y1aPxkHQVSa5ZCHb/gS2lV95U5C6K5zcMwZVc/q1OpPR5A4i7+s3NTR2jIzuh7dfLT260+HhHh/nlDENYDvVML3PHBxTzmjKgaN66XfZ/RrGRre3TwqvwEnMc6rrga97MZBAG8UdZmbMOwEeDhKhva6x9tagmorT8BqzNpD3w+L4+wKGXg==")
@@ -21,10 +23,14 @@ def decrypt_py():
     return aes.decrypt(cipher)
 
 if __name__ == "__main__":
-    python = timeit.timeit("decrypt_py()", setup="from __main__ import decrypt_py", number=100)
-    cpp = timeit.timeit("decrypt_bindings()", setup="from __main__ import decrypt_bindings", number=100)
+    py_test = timeit.timeit("decrypt_py()", setup="from __main__ import decrypt_py", number=RUN_COUNT)
+    cpp_test = timeit.timeit("decrypt_bindings()", setup="from __main__ import decrypt_bindings", number=RUN_COUNT)
+
+    # average times per run
+    py_avg = py_test / RUN_COUNT
+    cpp_avg = cpp_test / RUN_COUNT
 
     print(
-        f'Time taken to run python decryption: {python * 1000:.2f}ms\n'
-        f'Time taken to run c++ decryption: {cpp * 1000:.2f}ms'
+        f'Time taken to run python decryption: {py_avg * 1000:.2f}ms\n'
+        f'Time taken to run c++ decryption: {cpp_avg * 1000:.2f}ms'
     )
