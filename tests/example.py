a01554e87775b01cbeb9845446a81a4075b8d770
Rudolf Kolbe <rkolbe96@gmail.com>
2021-12-20 21:37:50 +0100

    Update example.py

--- tests/example.py
+++ tests/example.py
@@ -1,36 +1,46 @@
 from base64 import b64decode
 from py3rijndael import RijndaelCbc, ZeroPadding
 
-import cppdael
+from cppdael import encrypt, decrypt, decrypt_string, Rijndael, MODE_CBC
 import timeit
 
-RUN_COUNT = 1000 # how many times it should run the func to get an accurate average
+RUN_COUNT = 1000  # how many times it should run the func to get an accurate average
 
 key = "osu!-scoreburgr---------20211103"
 iv = b64decode("8cpBI5PTxOlfDqXN9dE2OBothneHAmQZlN99K6zEkxI=")
-cipher = b64decode("2rQUjbqnBfb5CdnfcdACG+IjeYlTkE9cHxD3y1aPxkHQVSa5ZCHb/gS2lV95U5C6K5zcMwZVc/q1OpPR5A4i7+s3NTR2jIzuh7dfLT260+HhHh/nlDENYDvVML3PHBxTzmjKgaN66XfZ/RrGRre3TwqvwEnMc6rrga97MZBAG8UdZmbMOwEeDhKhva6x9tagmorT8BqzNpD3w+L4+wKGXg==")
+cipher = b64decode(
+    "2rQUjbqnBfb5CdnfcdACG+IjeYlTkE9cHxD3y1aPxkHQVSa5ZCHb/gS2lV95U5C6K5zcMwZVc/q1OpPR5A4i7+s3NTR2jIzuh7dfLT260+HhHh/nlDENYDvVML3PHBxTzmjKgaN66XfZ/RrGRre3TwqvwEnMc6rrga97MZBAG8UdZmbMOwEeDhKhva6x9tagmorT8BqzNpD3w+L4+wKGXg=="
+)
+decrypted_value = b"62ec0dcd994ccd1b385f90c0a8b3c4d3:tsunyoku :1cb0428f1646ac39168121b322872824:57:2:0:14:2:1:17810:62:False:F:144:False:0:211127230146:20211103\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14\x14"
 
-def decrypt_bindings(): return cppdael.decrypt(cipher, key, iv)
+
+def decrypt_cpp_string():
+    return decrypt_string(cipher, key, iv)
+
+
+def decrypt_cpp_bytes():
+    return decrypt(MODE_CBC, 32, key.encode(), iv, cipher, 32)
+
+
+def decrypt_cpp_cls():
+    return Rijndael(MODE_CBC, 32, key.encode(), iv).decrypt(cipher)
+
 
 def decrypt_py():
-    aes = RijndaelCbc(
-        key=key,
-        iv=iv,
-        padding=ZeroPadding(32),
-        block_size=32
-    )
-
+    aes = RijndaelCbc(key=key, iv=iv, padding=ZeroPadding(32), block_size=32)
     return aes.decrypt(cipher)
 
+
 if __name__ == "__main__":
-    py_test = timeit.timeit("decrypt_py()", setup="from __main__ import decrypt_py", number=RUN_COUNT)
-    cpp_test = timeit.timeit("decrypt_bindings()", setup="from __main__ import decrypt_bindings", number=RUN_COUNT)
+    tests = []
+    for func in [decrypt_cpp_string, decrypt_cpp_bytes, decrypt_cpp_cls, decrypt_py]:
+        # 1. assert correctnes
+        fname = func.__name__
+        try:
+            assert decrypted_value == func()
+        except:
+            print(f"{fname} failed to produce a correct result")
 
-    # average times per run
-    py_avg = py_test / RUN_COUNT
-    cpp_avg = cpp_test / RUN_COUNT
-
-    print(
-        f'Time taken to run python decryption: {py_avg * 1000:.2f}ms\n'
-        f'Time taken to run c++ decryption: {cpp_avg * 1000:.2f}ms'
-    )
+        total = timeit.timeit(func, number=RUN_COUNT)
+        avg = total * 1000 / RUN_COUNT
+        print(f"Time taken to run {fname}: {avg:.2f}ms")
